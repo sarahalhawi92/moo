75c1edbd3614a07f49e0d7e1b0d7974b0fa40597
sarahalhawi <sarah.alhawi@masabi.com>
2017-04-22 19:41:02 +0100

    adding geoentity file back

---
+++ src/main/java/com/moo/fakeapitests/GeoEntity.java
@@ -0,0 +1,23 @@
+package com.moo.fakeapitests;
+
+public class GeoEntity {
+
+    private float lat;
+    private float lng;
+
+    public float getLat() {
+        return lat;
+    }
+
+    public void setLat(float lat) {
+        this.lat = lat;
+    }
+
+    public float getLng() {
+        return lng;
+    }
+
+    public void setLng(float lng) {
+        this.lng = lng;
+    }
+}
